d93751cf2dc4948a1f6634b9dd0a56ac709ee7ee
Aneesh Reddy <beabravedude@gmail.com>
2026-01-31 06:14:03 -0500

    feat: enhance Next.js configuration with standalone output and image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,29 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
+    reactCompiler: true,
+    output: 'standalone',
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '1gb',
+        },
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'radar.weather.gov',
+                port: '',
+                pathname: '**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+                port: '',
+                pathname: '**',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
